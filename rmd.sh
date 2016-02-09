3dd29f6945feaa9da46367ad9eacacd8627e1fdd
Grace Smith Vidaurre <gsmithvi@gmail.com>
2016-02-09 16:09:31 -0700

    Adding files in _includes, _layouts, _scss

--- rmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# http://jason.bryer.org/posts/2012-12-10/Markdown_Jekyll_R_for_Blogging.html
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-Rscript -e "source('$DIR/rmarkdown.r'); convertRMarkdown(images.dir='$DIR/images')"
